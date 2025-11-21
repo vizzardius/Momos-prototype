45d418e834c8a6d3177d0d92de481a3ccad6745d
vizzardius <vizzardiusgh14@gmail.com>
2025-11-21 23:12:41 +0200

    Add files via upload

---
+++ preloader.js
@@ -0,0 +1,18 @@
+let loadCircle = document.querySelector(".loadCircle")
+let welcomeText = document.querySelector(".welcomeText")
+let momosTitle = document.querySelector(".momosTitle")
+
+
+setTimeout(()=>{
+   loadCircle.style.animation = "scaling 0.6s ease"
+   loadCircle.style.transform = "scale(17)"
+},500)
+
+setTimeout(()=>{
+   welcomeText.style.animation = "fadeUp 1s ease forwards"
+},1500)
+
+setTimeout(()=>{
+   momosTitle.style.animation = "fadeUp 1s ease forwards"
+},2500)
+
